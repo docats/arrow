1de46d46227e7ccdea88277ac20a874d9af87a63
docats <j58701076@yahoo.com.tw>
2021-05-12 16:16:18 +0800

    陣列不寫死版

--- rect.js
+++ rect.js
@@ -116,32 +116,73 @@ function SvgPolygon() {
     } 
   }
 
+ 
 
+  const shape_array = {
+   polygon:[
+     {
+       name:"set_0",
+      //  coord:[[350,190],[400,150],[200,450],[100,170]]
+      coordX:[350,400,200,100],
+      coordY:[190,150,450,170],
+     },
+     {
+      name:"set_1",
+     coordX:[250,100,100,200],
+     coordY:[90,150,50,170],
+    },
+    {
+     name:"set_2",
+     coordX:[100,500,600,400],
+     coordY:[60,150,300,400],
+    }
+   ]
+ }
+
+//  var shapeX_0 = [[shape_array.polygon[0].coordX[0]],[shape_array.polygon[0].coordX[1]],[shape_array.polygon[0].coordX[2]],[shape_array.polygon[0].coordX[3]]];
+//  var shapeY_0 = [[shape_array.polygon[0].coordY[0]],[shape_array.polygon[1].coordY[1]],[shape_array.polygon[2].coordY[2]]];
+ 
+//  console.log("使用物件陣列取座標:"+ shapeX_0 +"  "+"使用物件陣列取座標:"+ shapeY_0);
+//  console.log("屬性:"+parseInt(shapeY_0));
+ 
+ 
 //按鈕0
-function typeZero(){
+function typeZero(a,b){
   var theSVG = document.getElementById("theSVG");
   var typeBtn_0 = document.getElementById("typeBtn_0").value;
   var stroke_01 = theSVG.style.stroke = "#fff";
   var fill_01 = theSVG.style.fill = "#fff";
+  var handle = document.getElementsByClassName("handle");
   
-  var handle = document.getElementsByClassName("handle")  
+  // var handle1 = document.getElementsByClassName("handle")[0].style; 
+  // console.log("我是:"+handle1); 
+
+  // console.log("這到底是:"+handle);
+
   if(typeBtn_0 == 0){
     //把手座標，必須在陣列裡設按鈕0的把手預設值，才不會被前一個的把手設好的座標值影響這次要設定的座標
-   var x = [handle[1].style.left="350px",handle[2].style.left="400px",handle[3].style.left="200px",handle[3].style.top="100px"]
-   var y = [handle[0].style.top="190px",handle[1].style.top="150px",handle[2].style.top="450px",handle[3].style.top="170px"];
+  //  var x = [handle[0].style.left="350px",handle[1].style.left="400px",handle[2].style.left="200px",handle[3].style.left="100px"];
+  // //  var y = [handle[0].style.top="190px",handle[1].style.top="150px",handle[2].style.top="450px",handle[3].style.top="170px"];
+   var x = [handle[0].style.left=[shape_array.polygon[0].coordX[0]]+"px",handle[1].style.left=[shape_array.polygon[0].coordX[1]]+"px",handle[2].style.left=[shape_array.polygon[0].coordX[2]]+"px",handle[3].style.left=[shape_array.polygon[0].coordX[3]]+"px"];
+   var y = [handle[0].style.top=[shape_array.polygon[0].coordY[0]]+"px",handle[1].style.top=[shape_array.polygon[0].coordY[1]]+"px",handle[2].style.top=[shape_array.polygon[0].coordY[2]]+"px",handle[3].style.top=[shape_array.polygon[0].coordY[3]]+"px"];
+   console.log("輸出座標x:"+x);
     //把取出來的座標放到pointX_small和pointY_small
    pointX_small = x;
    pointY_small = y;
   
     console.log("value:"+ typeBtn_0);
-    return;
+    // console.log("使用物件陣列取座標:"+ shapeX+"  "+"使用物件陣列取座標:"+ shapeY);
+    return 
   }else{
     console.log("1212");
   }
  
    console.log("this is typeZero");
+   return  typeZero(2,3)
+  
    
 }
+
 
 //按鈕1
  function typeOne(){
@@ -153,8 +194,8 @@ function typeZero(){
    
    if(typeBtn_1 == 1){
     //把手座標，必須在陣列裡設按鈕1的把手預設值，才不會被前一個的把手設好的座標值影響這次要設定的座標
-    var x = [handle[1].style.left="250px",handle[2].style.left="100px",handle[3].style.left="100px",handle[3].style.top="200px"]
-    var y = [handle[0].style.top="90px",handle[1].style.top="150px",handle[2].style.top="50px",handle[3].style.top="170px"];
+    var x = [handle[0].style.left=[shape_array.polygon[1].coordX[0]]+"px",handle[1].style.left=[shape_array.polygon[1].coordX[1]]+"px",handle[2].style.left=[shape_array.polygon[1].coordX[2]]+"px",handle[3].style.left=[shape_array.polygon[1].coordX[3]]+"px"];
+    var y = [handle[0].style.top=[shape_array.polygon[1].coordY[0]]+"px",handle[1].style.top=[shape_array.polygon[1].coordY[1]]+"px",handle[2].style.top=[shape_array.polygon[1].coordY[2]]+"px",handle[3].style.top=[shape_array.polygon[1].coordY[3]]+"px"];
 
     //把取出來的座標放到pointX_small和pointY_small
     pointX_small = x;
@@ -177,8 +218,8 @@ function typeTwo(){
   if(typeBtn_2 == 2){
     console.log('value:'+typeBtn_2);
      //把手座標，預設是loading的值(也就是全域變數預設的座標X和Y)，因故必須在陣列裡設按鈕2的把手預設值，才會不離開function被上面的把手設好的全域變數蓋過去
-    var x = [handle[1].style.left="100px",handle[2].style.left="500px",handle[3].style.left="600px",handle[3].style.top="400px"]
-    var y = [handle[0].style.top="60px",handle[1].style.top="150px",handle[2].style.top="300px",handle[3].style.top="400px"];
+    var x = [handle[0].style.left=[shape_array.polygon[2].coordX[0]]+"px",handle[1].style.left=[shape_array.polygon[2].coordX[1]]+"px",handle[2].style.left=[shape_array.polygon[2].coordX[2]]+"px",handle[3].style.left=[shape_array.polygon[2].coordX[3]]+"px"];
+    var y = [handle[0].style.top=[shape_array.polygon[2].coordY[0]]+"px",handle[1].style.top=[shape_array.polygon[2].coordY[1]]+"px",handle[2].style.top=[shape_array.polygon[2].coordY[2]]+"px",handle[3].style.top=[shape_array.polygon[2].coordY[3]]+"px"];
    
     //把取出來的座標放到pointX_small和pointY_small
     pointX_small = x;
